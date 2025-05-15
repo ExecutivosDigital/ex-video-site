39fbbe777eb77123c24a940bfde9552b81281bcf
VictorHOgawa <victor.ogawa@gmail.com>
2025-05-15 16:55:20 -0300

    build(build): first commit

---
+++ src/app/(private)/components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <div className="flex h-20 w-full items-center justify-between border-b border-b-zinc-200 px-4 xl:h-28 xl:px-8">
+      <span className="font-manrope hidden text-2xl font-bold xl:block">
+        SOLUÇÕES SOB MEDIDA
+      </span>
+      <Image
+        src="/dark-logo.png"
+        alt=""
+        width={1000}
+        height={500}
+        className="h-16 w-max object-contain xl:h-20"
+      />
+      <button className="font-manrope flex items-center gap-2 rounded-lg border border-black px-2 py-2 text-sm font-bold text-black xl:px-8 xl:text-base">
+        Fale Conosco
+        <Image
+          src="/whats-icon.png"
+          alt=""
+          width={250}
+          height={250}
+          className="h-4 w-4 xl:h-6 xl:w-6"
+        />
+      </button>
+    </div>
+  );
+}
